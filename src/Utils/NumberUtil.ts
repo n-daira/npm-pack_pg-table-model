c842c01f0bd236e77299814a1515424c61a7382e
ndaira <nakadaira44@yahoo.co.jp>
2025-04-09 14:11:29 +0900

    complete Utils

---
+++ src/Utils/NumberUtil.ts
@@ -0,0 +1,25 @@
+export default class NumberUtil {
+
+    /**
+     * Checks if the given value is a number.
+     * 与えられた値が数値であるかどうかを確認します。
+     * @param value - The value to be checked. ���認する値。
+     * @returns {boolean} - Whether the value is a number. ��が数値であるかどうか。
+     */
+    static isNumber(value: any) {
+
+        if (value == null) {
+            return false;
+        } else if (value instanceof Date) { 
+            return false;
+        } else if (value instanceof Array) {
+            return false;
+        } else if (typeof(value) == 'string') {
+            if (value == "") {
+                return false;
+            }
+        }
+
+        return isNaN(Number(value)) == false;
+    }
+}
